b5ef95cab1513494faf377a119e477d3d39676a8
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-06-09 01:29:29 +0100

    cool

--- tauri-app/src/App.jsx
+++ tauri-app/src/App.jsx
@@ -1,102 +1,78 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-import { invoke } from '@tauri-apps/api/tauri';
-// import './App.css';
-// import Home from './pages/Home/Home';
-
-// function App() {
-//   const [greetMsg, setGreetMsg] = useState('');
-//   const [name, setName] = useState('');
-
-//   async function greet() {
-//     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-//     setGreetMsg(await invoke('greet', { name }));
-//   }
-
-//   return (
-//     <div className='container'>
-//       <h1>Welcome to Tauri!</h1>
-
-//       <div className='row'>
-//         <a
-//           href='https://vitejs.dev'
-//           target='_blank'>
-//           <img
-//             src='/vite.svg'
-//             className='logo vite'
-//             alt='Vite logo'
-//           />
-//         </a>
-//         <a
-//           href='https://tauri.app'
-//           target='_blank'>
-//           <img
-//             src='/tauri.svg'
-//             className='logo tauri'
-//             alt='Tauri logo'
-//           />
-//         </a>
-//         <a
-//           href='https://reactjs.org'
-//           target='_blank'>
-//           <img
-//             src={reactLogo}
-//             className='logo react'
-//             alt='React logo'
-//           />
-//         </a>
-//       </div>
-
-//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-//       <Home />
-//       <form
-//         className='row'
-//         onSubmit={(e) => {
-//           e.preventDefault();
-//           greet();
-//         }}>
-//         <input
-//           id='greet-input'
-//           onChange={(e) => setName(e.currentTarget.value)}
-//           placeholder='Enter a name...'z
-//         />
-//         <button type='submit'>Greet</button>
-//       </form>
-
-//       <p>{greetMsg}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React, { useRef } from 'react';
-import ReactDOM from 'react-dom';
-
-import Editor from '@monaco-editor/react';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+import { open } from "@tauri-apps/api/dialog";
+import { appDir } from "@tauri-apps/api/path";
+// Open a selection dialog for directories
+import { readDir, BaseDirectory } from "@tauri-apps/api/fs";
 
 function App() {
-  const editorRef = useRef(null);
+  const [folderDir, setFolderDir] = useState("");
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
 
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
+  async function readFolder(path) {
+    if (!path) return;
+    const entries = await readDir(path, { recursive: true });
+    processEntries(entries);
+    function processEntries(entries) {
+      for (const entry of entries) {
+        console.log(`Entry: ${entry.path}`);
+        if (entry.children) {
+          processEntries(entry.children);
+        }
+      }
+    }
   }
 
-  function showValue() {
-    alert(editorRef.current.getValue());
+  async function openFolder() {
+    const selected = await open({
+      directory: true,
+      // multiple: true,
+      recursive: true,
+      defaultPath: await appDir(),
+    });
+    if (Array.isArray(selected)) {
+      // user selected multiple directories
+    } else if (selected === null) {
+      // user cancelled the selection
+    } else {
+      setFolderDir(selected);
+      readFolder(selected);
+      // traverseFolder(path)
+      // user selected a single directory
+    }
   }
 
   return (
-    <>
-      <button onClick={showValue}>Show value</button>
-      <Editor
-        height='90vh'
-        defaultLanguage='javascript'
-        defaultValue='// some comment'
-        onMount={handleEditorDidMount}
-      />
-    </>
+    <div className="container">
+      <h1>Code Co-Pilot</h1>
+      <p>Chose a folder to get started</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          openFolder();
+        }}
+      >
+        <PathCard placeholder={folderDir ? folderDir : ""} />
+        <button type="submit">Open</button>
+      </form>
+    </div>
   );
 }
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+function PathCard({ props, placeholder }) {
+  return (
+    <input
+      style={{ boxShadow: placeholder && "0px 0px 5px green" }}
+      disabled
+      id="greet-input"
+      placeholder={placeholder || "Select a path..."}
+    />
+  );
+}
+
+export default App;
--- tauri-app/src/main.jsx
+++ tauri-app/src/main.jsx
@@ -1,36 +1,10 @@
-import { invoke } from '@tauri-apps/api';
-import React, { useRef } from 'react';
-import ReactDOM from 'react-dom';
-import Editor from '@monaco-editor/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-function App() {
-  const editorRef = useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
-
-  async function showValue() {
-    // now we can call our Command!
-    // Right-click the application background and open the developer tools.
-    // You will see "Hello, World!" printed in the console!
-    invoke('greet', { name: 'World' })
-      // `invoke` returns a Promise
-      .then((response) => alert(response));
-  }
-
-  return (
-    <>
-      <button onClick={showValue}>Show value</button>
-      <Editor
-        height='90vh'
-        defaultLanguage='javascript'
-        defaultValue='// some comment'
-        onMount={handleEditorDidMount}
-      />
-    </>
-  );
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
